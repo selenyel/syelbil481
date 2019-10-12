4aa6de5bd85d32625d14a13fdf4ea59f1ea8dcc2
selenyel <syel@etu.edu.tr>
2019-10-12 22:44:32 +0300

    I have started my awesome project!!It will be great

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+
+
+    public void testFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));       assertTrue(new App().search(array, 4));
+    }
+
+    public void testNotFound() {
+        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));       assertFalse(new App().search(array, 5));
+    }
+
+    public void testEmptyArray() {
+        ArrayList<Integer> array = new ArrayList<>();       assertFalse(new App().search(array, 1));
+    }
+
+    public void testNull() {
+        assertFalse(new App().search(null, 1));
+    }
+
+}
